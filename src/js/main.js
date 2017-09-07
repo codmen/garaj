92ee4e214c49dafe94af551ad87494d52b965da2
gitbooster <webtheory@mail.ru>
2017-09-08 02:08:04 +0300

    Header is done

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+/* Third party */
+//= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../bower_components/bootstrap/js/popper.min.js
+//= ../../bower_components/bootstrap/js/bootstrap.js
+
+
+/* Custom*/
+//= partials/prettify.js
+//= partials/helper.js
+
+
---
+++ src/js/partials/helper.js
@@ -0,0 +1,10 @@
+!function($){$(function(){window.prettyPrint&&prettyPrint()})}(window.jQuery) 
+$(document).ready(function() {
+    $("pre").each(function() {
+        	$(this).removeClass();
+            $(this).addClass('prettyprint linenums');
+        
+    });
+    prettyPrint();
+});
+

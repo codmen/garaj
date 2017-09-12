e40dc16bd261af614b5ab7cb07d3b29a09e5d860
gitbooster <webtheory@mail.ru>
2017-09-12 20:10:43 +0300

    Add tables & Add slider

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,10 @@
 //= ../../bower_components/jquery/dist/jquery.min.js
 //= ../../bower_components/bootstrap/js/popper.min.js
 //= ../../bower_components/bootstrap/js/bootstrap.js
+//= ../../bower_components/owl.carousel/dist/owl.carousel.min.js
 
 
 /* Custom*/
-//= partials/prettify.js
 //= partials/helper.js
 
 
